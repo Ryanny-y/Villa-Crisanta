a3dc6dcd6f9a7166a87263e6a6a2b25efe3d32e3
ryanny <ryannymromero@gmail.com>
2024-05-13 08:21:39 +0800

    Add starting files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'gold-1': "#DD9603",
+        'black-1': '#202020',
+        'white-1': '#FAFAFA'
+      }
+    },
+  },
+  plugins: [],
+}
+
