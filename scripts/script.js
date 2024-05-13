d52af0012788e4305a6287eec9b8445b1470eed5
ryanny <ryannymromero@gmail.com>
2024-05-13 10:41:06 +0800

    Add swiper but startup

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const photoGallery = new Swiper('.photo-gallery-swiper', {
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
