1eba936b3a4b046a81961266ffdb204bb5394db3
ryanny <ryannymromero@gmail.com>
2024-05-15 11:09:58 -0800

    Make the spreadsheet working and the gfrom

---
+++ scripts/backend/sign-in.js
@@ -0,0 +1,69 @@
+export function oauthSignIn() {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': '13411952836-3tpdg6hfrbpr3qgspqgo8kf6t9vjak1r.apps.googleusercontent.com',
+                'redirect_uri': 'https://villacrisanta-111.netlify.app/villa-1.html',
+                'response_type': 'token',
+                'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}
+
+export function handleRequest(signInBtn) {
+  let params = {};
+
+  let regex = /[?&]+([^=&]+)=([^&]*)/g; // Updated regex pattern
+  let url = window.location.href; // Using window.location.href to get the current URL
+
+  let m;
+  while ((m = regex.exec(url)) !== null) {
+      params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+  }
+
+  if (Object.keys(params).length > 0) {
+      localStorage.setItem('authInfo', JSON.stringify(params));
+  }
+
+  window.history.pushState({}, document.title, '/' + 'villa-1.html');
+
+  let info = JSON.parse(localStorage.getItem('authInfo'));
+
+  if (info && info['access_token']) {
+    signInBtn.parentElement.classList.add('hidden');
+
+    if(innerWidth > 768) {
+      setTimeout(() => {
+        window.scrollTo(0, document.body.scrollHeight - 200);
+      }, 1000)
+    } else {
+      setTimeout(() => {
+        window.scrollTo(0, document.body.scrollHeight - 1100);
+      }, 1000)
+    }
+    
+  } else {
+    document.querySelector('.indicator').innerHTML = 'Not Signed In <br> <span class="text-sm">Please sign in to fill up the form</span>';
+  }
+
+}
--- scripts/villa1.js
+++ scripts/villa1.js
@@ -1,5 +1,7 @@
 import { hamburger, headerBgTransition } from "./utils/handleTransitions.js";
 import { roomsSwiper } from './utils/swiper.js';
+import { handleRequest, oauthSignIn } from "./backend/sign-in.js";
+import { villaSpreadSheet } from "./backend/spreadsheet.js";
 
 window.addEventListener("load", () => {
   setTimeout(() => {
@@ -17,78 +19,8 @@ window.addEventListener("load", () => {
   signInBtn.addEventListener('click', oauthSignIn);
   // const logoutBtn = document.querySelector('.logout-btn');
   // logoutBtn.addEventListener('click', logout);
-
-  function oauthSignIn() {
-    // Google's OAuth 2.0 endpoint for requesting an access token
-
-    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
   
-    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-    var form = document.createElement('form');
-    form.setAttribute('method', 'GET'); // Send as a GET request.
-    form.setAttribute('action', oauth2Endpoint);
-  
-    // Parameters to pass to OAuth 2.0 endpoint.
-    var params = {'client_id': '13411952836-3tpdg6hfrbpr3qgspqgo8kf6t9vjak1r.apps.googleusercontent.com',
-                  'redirect_uri': 'https://villacrisanta-111.netlify.app/villa-1.html',
-                  'response_type': 'token',
-                  'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
-                  'include_granted_scopes': 'true',
-                  'state': 'pass-through value'};
-  
-    // Add form parameters as hidden input values.
-    for (var p in params) {
-      var input = document.createElement('input');
-      input.setAttribute('type', 'hidden');
-      input.setAttribute('name', p);
-      input.setAttribute('value', params[p]);
-      form.appendChild(input);
-    }
-  
-    // Add form to page and submit it to open the OAuth 2.0 endpoint.
-    document.body.appendChild(form);
-    form.submit();
-  }
-  
-  function handleRequest() {
-    let params = {};
-
-    let regex = /[?&]+([^=&]+)=([^&]*)/g; // Updated regex pattern
-    let url = window.location.href; // Using window.location.href to get the current URL
-
-    let m;
-    while ((m = regex.exec(url)) !== null) {
-        params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-    }
-
-    if (Object.keys(params).length > 0) {
-        localStorage.setItem('authInfo', JSON.stringify(params));
-    }
-
-    window.history.pushState({}, document.title, '/' + 'villa-1.html');
-
-    let info = JSON.parse(localStorage.getItem('authInfo'));
-
-    if (info && info['access_token']) {
-      signInBtn.parentElement.classList.add('hidden');
-
-      if(innerWidth > 768) {
-        setTimeout(() => {
-          window.scrollTo(0, document.body.scrollHeight - 200);
-        }, 1000)
-      } else {
-        setTimeout(() => {
-          window.scrollTo(0, document.body.scrollHeight - 1100);
-        }, 1000)
-      }
-      
-    } else {
-      document.querySelector('.indicator').innerHTML = 'Not Signed In <br> <span class="text-sm">Please sign in to fill up the form</span>';
-    }
-
-  }
-  
-  handleRequest();
+  handleRequest(signInBtn);
 
   function logout() {
     // Clear OAuth information from localStorage
@@ -96,7 +28,11 @@ window.addEventListener("load", () => {
     
     // Redirect to Google logout URL
     window.location.href = 'https://villacrisanta-111.netlify.app/villa-1.html';
-}
+  }
+
+
+  // SPREAD SHEET
+  document.getElementById("form").addEventListener("submit", villaSpreadSheet);
   
 
   // SWIPERS
